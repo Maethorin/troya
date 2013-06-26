f969852db2b6bc0fac630d0210bb6345d7d372ad
Márcio Duarte <maethorin@gmail.com>
2013-06-25 23:30:40 -0300

    Fase 4

--- app/handlers/lateral.py
+++ app/handlers/lateral.py
@@ -10,14 +10,16 @@ class LateralHandler(tornado.web.RequestHandler):
     def get(self):
         mensagem = "Comando executado com sucesso. Verifique a ferramenta de monitoração."
         status = "success"
+        exit_code = 0
         try:
             sh.apt_get("install", "rdesktop", y=True)
-            output = sh.rdesktop("172.20.10.13", u="Administrador", p="Troca123", a="16", k="none", _bg=True)
-            if output.exit_code != 0:
+            output = sh.rdesktop("172.20.10.13", u="Administrador", p="Troca123", a="16", k="none")
+            exit_code = output.exit_code
+            if exit_code != 0:
                 mensagem = "Ocorreu um erro na execução do rdesktop e retornou %s" % output.exit_code
                 status = "error"
         except sh.ErrorReturnCode, ex:
-            mensagem = "Ocorreu um erro na execução: %s" % ex.stderr
+            mensagem = "Ocorreu um erro na execução: %s - Exit code: %s" % (ex.stderr, exit_code)
             status = "error"
 
         kwargs = {
